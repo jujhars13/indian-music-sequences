ffa690b1134ddce5f1dbb90f2e78bbb4cc6294fb
Jujhar Singh <jujhar@jujhar.com>
2023-02-26 06:23:21 +0000

    gets webpack working

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const targetDir = path.resolve(__dirname, "docs");
 
 module.exports = {
   entry: "./src/index.js",
-  //mode: 'production',
-  mode: "development",
+  mode: process.env?.NODE_ENV || "development",
   output: {
     filename: "js/main.js",
-    path: path.resolve(__dirname, "docs"),
+    path: targetDir,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
     }),
     new CopyWebpackPlugin({
-      patterns: [{ from: "src/css", to: path.resolve(__dirname, "docs/css") }],
+      patterns: [
+        { from: "src/css", to: `${targetDir}/css` },
+        { from: "src/CNAME", to: `${targetDir}` },
+      ],
     }),
   ],
 };
